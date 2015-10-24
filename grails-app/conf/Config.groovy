2105329b06ae89852300cb80fe6593a61d144d4e
turboDi <turbo_di@outlook.com>
2015-10-25 00:43:27 +0500

    Set token expiration time to 5 min for testing

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -156,6 +156,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 grails.plugin.springsecurity.filterChain.chainMap = [
         '/**': 'JOINED_FILTERS,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'
 ]
+grails.plugin.springsecurity.rest.token.storage.jwt.expiration = 300
 
 grails.plugin.likeable.liker.className = 'ru.jconsulting.igetit.Person'
 grails.plugin.likeable.liker.evaluator = { delegate.getAuthenticatedUser() }
