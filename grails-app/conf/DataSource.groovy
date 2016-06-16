bb9826109ec45850f1a2d73e03acf8cadbda9697
turboDi <turbo_di@outlook.com>
2016-06-16 22:23:18 +0500

    production DB properties

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -39,6 +39,25 @@ environments {
             url = "jdbc:postgresql://${env('RDS_HOSTNAME')}:5432/${env('RDS_DB_NAME')}"
             username = env('RDS_USERNAME')
             password = env('RDS_PASSWORD')
+            properties {
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
             dialect = TableNameSequencePostgresDialect
         }
     }
